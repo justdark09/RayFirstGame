547f5162222c6e90c2b4eb6a0411c6931ea2d110
justdark09 <zacharyjyoungblood@gmail.com>
2024-12-01 23:01:58 -0600

    complete re-write of the code. makes more sense this way, utilizing structs to hold the values of the texture Rectangle source and the max amount of frames for that animation. only one sprite sheet for every animation, rather than loading a new texture for every animation.

--- main.cpp
+++ main.cpp
@@ -1,120 +1,79 @@
 #include <iostream>
 #include "raylib.h"
 
-
 int main()
 {
-    constexpr float winHeight = 900;
-    constexpr float winWidth = 1600;
-    InitWindow(winWidth,winHeight,"Platformer");
+    InitWindow(1600, 900, "Knight Game");
     SetTargetFPS(60);
 
-    Texture2D knightIdle = LoadTexture("assets/sprites/FreeKnight_v1/Colour2/NoOutline/120x80_PNGSheets/_IdleH.png");
-    Texture2D knightAttack2 = LoadTexture("assets/sprites/FreeKnight_v1/Colour2/NoOutline/120x80_PNGSheets/_Attack2H.png");
-    Texture2D knightAttackNoMove = LoadTexture("assets/sprites/FreeKnight_v1/Colour2/NoOutline/120x80_PNGSheets/_Attack2NoMovementH.png");
+    Texture2D knightSheet = LoadTexture("assets/sprites/knight_sheet.png");
+    const float frameHeight = knightSheet.height/30;
+    float frameWidth = knightSheet.width/12;
+
+    // holds the source Rectangle and the MaxFrames for each object of a texture
+    struct AnimationStruct {
+        Rectangle animRec;
+        int maxFrames;
+    };
 
 
-    Texture2D currentTexture = knightIdle;
+    // make structs to hold each animation's values.
+    // (i.e. where to get the src rectangle from, the max amount of frames that animatiion has, etc.)
+    AnimationStruct idle_struct{};
 
-    const int knightScaleFactor = 5;
-
-    const auto currentTextureFrameWidth = static_cast<float>(currentTexture.width / 10.0);
-    const int maxIdleFrames = knightIdle.width / static_cast<int>(currentTextureFrameWidth);
-
-
-    /*
-    const auto attack2FrameWidth = static_cast<float>(knightAttack2.width / 6.0);
-    const int maxAttack2Frames = knightAttack2.width / static_cast<int>(attack2FrameWidth);
-
-    const auto noMoveAttackFrameWidth = static_cast<float>(knightAttackNoMove.width / 6.0);
-    const int maxNoMoveFrames = knightAttackNoMove.width / static_cast<int>(noMoveAttackFrameWidth);
-    */
-
-
-    const float knightHeightScaled = knightIdle.height * knightScaleFactor;
-    const float knightWidthScaled = currentTextureFrameWidth * knightScaleFactor;
-
+    AnimationStruct attack_struct{};
 
     float timer = 0.0f;
-    int frameIdle = 0;
-    int frameAtt = 0;
-    int frameNoMove = 0;
-
-    float idleStartPos = winWidth/2;
+    int frame = 0;
+    int frame1 = 0;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
-        ClearBackground(PURPLE);
+        ClearBackground(BLACK);
 
         timer += GetFrameTime();
 
-        if (timer >= 0.1f) {
+        // increases the frame value every frameTime (tf does that actually mean)
+        if (timer > .25f) {
             timer = 0.0f;
-            frameIdle += 1;
-            frameAtt += 1;
-            frameNoMove += 1;
+            frame += 1;
+            frame1 += 1;
         }
 
-        frameIdle = frameIdle % maxIdleFrames;
-        //frameAtt = frameAtt % maxAttack2Frames;
-        //std::cout<<frame<<std::endl;
 
-        DrawLine(0,winHeight/2,winWidth,winHeight/2, RED);
-        DrawLine(winWidth/2,0,winWidth/2, winHeight, RED);
+        // initialize the animation structs
+        idle_struct.maxFrames = 10;
+        idle_struct.animRec = { frameWidth*frame, frameHeight*16, frameWidth, frameHeight };
 
+        attack_struct.maxFrames = 4;
+        attack_struct.animRec = { frameWidth*frame1, frameHeight, frameWidth, frameHeight };
 
+        int currentMaxFrames = idle_struct.maxFrames;
 
-        if (IsKeyDown(KEY_D)) {
-            if (IsKeyDown(KEY_LEFT_SHIFT)) {
-                idleStartPos += GetFrameTime() * 300;
-                currentTexture = knightAttackNoMove;
-            } else {
-                idleStartPos += GetFrameTime() * 100;
-                currentTexture = knightIdle;
-            }
-        } else {
-            currentTexture = knightIdle;
-        }
+        int tempMaxFrames = attack_struct.maxFrames;
 
-        if (IsKeyDown(KEY_A)) {
-            if (IsKeyDown(KEY_LEFT_SHIFT)) {
-                idleStartPos -= GetFrameTime() * 300;
-                currentTexture = knightAttackNoMove;
-            } else {
-                idleStartPos -= GetFrameTime() * 100;
-                currentTexture = knightIdle;
-            }
-        } else {
-            currentTexture = knightIdle;
-        }
+        frame = frame % currentMaxFrames;
+        frame1 = frame1 % tempMaxFrames;
 
         DrawTexturePro(
-            currentTexture,
-            Rectangle{ (currentTextureFrameWidth*frameIdle), 0, currentTextureFrameWidth, static_cast<float>(knightIdle.height) },
-            Rectangle{ idleStartPos,winHeight/2, knightWidthScaled, knightHeightScaled },
-            Vector2{ currentTextureFrameWidth/2,static_cast<float>(knightIdle.height)/2}, 0, WHITE);
-
-        /*
-        DrawTexturePro(
-            knightAttack2,
-            Rectangle{ (attack2FrameWidth*frameAtt), 0, attack2FrameWidth, static_cast<float>(knightAttack2.height)},
-            Rectangle{winWidth/4,winHeight/2, attack2FrameWidth*5, (float)knightAttack2.height*5},
-            Vector2{ attack2FrameWidth/2,static_cast<float>(knightAttack2.height)/2}, 0, WHITE);
+            knightSheet,
+            idle_struct.animRec,
+            Rectangle{ 800, 450, frameWidth*5, frameHeight*5},
+            Vector2{0,0},
+            0.f,
+            WHITE);
 
         DrawTexturePro(
-            knightAttackNoMove,
-            Rectangle{ noMoveAttackFrameWidth*frameNoMove, 0, noMoveAttackFrameWidth, static_cast<float>(knightAttackNoMove.height) },
-            Rectangle{ winWidth-(winWidth/4),winHeight/2, noMoveAttackFrameWidth*5, (float)knightAttackNoMove.height*5},
-            Vector2{ noMoveAttackFrameWidth/2, static_cast<float>(knightAttackNoMove.height)/2}, 0, WHITE);
-
-        */
+            knightSheet,
+            attack_struct.animRec,
+            Rectangle{ 400, 450, frameWidth*5, frameHeight*5},
+            Vector2{0,0},
+            0.f,
+            WHITE);
 
         EndDrawing();
     }
-    UnloadTexture(knightIdle);
-    UnloadTexture(knightAttack2);
-    UnloadTexture(knightAttackNoMove);
-    CloseWindow();
+    UnloadTexture(knightSheet);
     return 0;
 }
