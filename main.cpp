1caefc409bb38a8739933d21a211cddf03e4dc25
justdark09 <zacharyjyoungblood@gmail.com>
2024-12-02 22:40:18 -0600

    added movement, more animation structs, flipped frames. edited the animation struct to take the height of the animation only rather than the whole rectangle. see the issues tab (or the TODO).

--- main.cpp
+++ main.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
 #include "raylib.h"
 
+#define SCREEN_WIDTH 1600
+#define SCREEN_HEIGHT 900
+
+int printThanks()
+{
+    std::cout<<"\nThanks for PlayTesting!\n";
+    return 0;
+}
+
 int main()
 {
-    InitWindow(1600, 900, "Knight Game");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Knight Game");
     SetTargetFPS(60);
 
     Texture2D knightSheet = LoadTexture("assets/sprites/knight_sheet.png");
-    const float frameHeight = knightSheet.height/30;
-    float frameWidth = knightSheet.width/12;
+    const float frameHeight = static_cast<float>(knightSheet.height)/30;
+    const float frameWidth = static_cast<float>(knightSheet.width)/12;
+    const float flippedFrame = -frameWidth;
+    float currentFrame = frameWidth;
 
     // holds the source Rectangle and the MaxFrames for each object of a texture
     struct AnimationStruct {
-        Rectangle animRec;
+        float animationHeight;
         int maxFrames;
     };
 
-
     // make structs to hold each animation's values.
-    // (i.e. where to get the src rectangle from, the max amount of frames that animatiion has, etc.)
+    // (i.e. where to get the src rectangle from, the max amount of frames that animation has, etc.)
+    AnimationStruct attack1_struct{};
+    AnimationStruct attack2_struct{};
+    AnimationStruct run_struct{};
     AnimationStruct idle_struct{};
+    AnimationStruct crouch_struct{};
 
-    AnimationStruct attack_struct{};
+    // placeholder struct so the textures can be changed
+    AnimationStruct current_struct{};
+
+    int currentMaxFrames;
 
     float timer = 0.0f;
     int frame = 0;
-    int frame1 = 0;
+
+    float spriteX = 0;
+    float spriteY = 0;
+
+    std::cout<<spriteX<<", "<<spriteY<<std::endl;
+    std::cout<<frameWidth<<", "<<frameHeight<<std::endl;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -35,45 +57,92 @@ int main()
         timer += GetFrameTime();
 
         // increases the frame value every frameTime (tf does that actually mean)
-        if (timer > .25f) {
+        if (timer > .12f) {
             timer = 0.0f;
             frame += 1;
-            frame1 += 1;
         }
 
 
         // initialize the animation structs
-        idle_struct.maxFrames = 10;
-        idle_struct.animRec = { frameWidth*frame, frameHeight*16, frameWidth, frameHeight };
+        // !! maxFrames should equal the number of frames the animation has MINUS ONE !!
+        idle_struct.maxFrames = 9;
+        idle_struct.animationHeight = frameHeight*16.f;
 
-        attack_struct.maxFrames = 4;
-        attack_struct.animRec = { frameWidth*frame1, frameHeight, frameWidth, frameHeight };
+        attack2_struct.maxFrames = 5;
+        attack2_struct.animationHeight = frameHeight*2.f;
 
-        int currentMaxFrames = idle_struct.maxFrames;
+        attack1_struct.maxFrames = 3;
+        attack1_struct.animationHeight = (float)frameHeight;
 
-        int tempMaxFrames = attack_struct.maxFrames;
+        run_struct.maxFrames = 9;
+        run_struct.animationHeight = frameHeight*20.f;
+
+        crouch_struct.maxFrames = 3;
+        crouch_struct.animationHeight = frameHeight*7;
+
+        const float runMultiplier = 1.25;
+        const float crouchMultiplier = .5;
+
+
+
+        if (IsKeyDown(KEY_D)) {
+            currentFrame = frameWidth;
+
+            if (IsKeyDown(KEY_LEFT_SHIFT)) {
+                spriteX += 5 * runMultiplier;
+            }
+
+            if (IsKeyDown(KEY_S)) {
+                spriteX += 5 * crouchMultiplier;
+                current_struct = crouch_struct;
+            } else {
+                spriteX += 5;
+                current_struct = run_struct;
+            }
+
+        } else if (IsKeyDown(KEY_A)) {
+            currentFrame = flippedFrame;
+
+            if (IsKeyDown(KEY_LEFT_SHIFT)) {
+                spriteX -= 5 * runMultiplier;
+            }
+
+            if (IsKeyDown(KEY_S)) {
+                spriteX -= 5 * crouchMultiplier;
+                current_struct = crouch_struct;
+            } else {
+                spriteX -= 5;
+                current_struct = run_struct;
+                currentFrame = flippedFrame;
+            }
+
+        } else {
+            current_struct = idle_struct;
+        }
+
+
+
+
+        currentMaxFrames = current_struct.maxFrames;
 
         frame = frame % currentMaxFrames;
-        frame1 = frame1 % tempMaxFrames;
+
 
         DrawTexturePro(
             knightSheet,
-            idle_struct.animRec,
-            Rectangle{ 800, 450, frameWidth*5, frameHeight*5},
+            Rectangle{ frameWidth*frame, current_struct.animationHeight, currentFrame, frameHeight },
+            Rectangle{ spriteX+(frameWidth*5),spriteY+(frameHeight*5), frameWidth*5, frameHeight*5 },
             Vector2{0,0},
             0.f,
             WHITE);
 
-        DrawTexturePro(
-            knightSheet,
-            attack_struct.animRec,
-            Rectangle{ 400, 450, frameWidth*5, frameHeight*5},
-            Vector2{0,0},
-            0.f,
-            WHITE);
+            // todo: spriteX and spriteY do not work with SCREEN_HEIGHT or SCREEN_WIDTH
+            //  causing the sprite to not be drawn on the screen in the first place
 
         EndDrawing();
+
     }
     UnloadTexture(knightSheet);
+    printThanks();
     return 0;
 }
